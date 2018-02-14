60776c21f4818cf600f89b72364c6966a2e0211f
Pero Brbora <pero.brbora@gmail.com>
2018-02-14 23:30:33 +0100

    Exclude html purifier

--- php/sami.php
+++ php/sami.php
@@ -6,13 +6,13 @@ use Symfony\Component\Finder\Finder;
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->exclude('HtmlPurifier')
-    ->exclude('SlugGenerator')
+    ->exclude('htmlpurifier')
+    ->exclude('sluggenerator')
     ->in(__DIR__ . '/src');
 
 return new Sami($iterator, array(
     'title' => 'Sustav za upravljanje sadržajem',
-    'build_dir' => '../doc/api',
-    'cache_dir' => '../doc/cache',
+    'build_dir' => '../docs/api',
+    'cache_dir' => '../docs/cache',
     'default_opened_level' => 1
 ));
