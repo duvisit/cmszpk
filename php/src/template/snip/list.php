9a198aa71c4c6763cbcd03fa8f06c49e9f5c9f8a
Pero Brbora <pero.brbora@gmail.com>
2018-02-27 18:45:58 +0100

    Fix array keys

--- php/src/template/snip/list.php
+++ php/src/template/snip/list.php
@@ -2,12 +2,12 @@
 <?php foreach ( $list as $item ) { ?>
 <article class="uk-article">
 <h2 class="uk-article-title">
-<a class="uk-link" href="<?= ( $listhref . $item[slug] ) ?>">
-<?= escapeOutput( $item[title] ) ?>
+<a class="uk-link" href="<?= ( $listhref . $item['slug'] ) ?>">
+<?= escapeOutput( $item['title'] ) ?>
 </a>
 </h2>
-<p class="uk-article-meta"><?= escapeOutput( $item[datum] ) ?></p>
-<p class="uk-article-lead"><?= escapeOutput( $item[summary] ) ?></p>
+<p class="uk-article-meta"><?= escapeOutput( $item['datum'] ) ?></p>
+<p class="uk-article-lead"><?= escapeOutput( $item['summary'] ) ?></p>
 </article>
 <hr class="uk-article-divider">
 <?php }
