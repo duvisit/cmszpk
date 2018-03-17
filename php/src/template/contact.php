74a8b4a5d47076b2597a18f6b5f5f798999867f9
Pero Brbora <pero.brbora@gmail.com>
2018-03-17 20:48:15 +0100

    Fix array index

--- php/src/template/contact.php
+++ php/src/template/contact.php
@@ -3,16 +3,16 @@
 <?php include __DIR__.'/snip/googlemap.php'; ?>
 <div class="uk-block uk-block-default">
 <div class="uk-text-center">
-<h1><?= \Sustav\Funkcije::escapeOutput( $site[name] ) ?></h1>
-<h2><?= \Sustav\Funkcije::escapeOutput( $site[description] ) ?></h2>
+<h1><?= \Sustav\Funkcije::escapeOutput( $site['name'] ) ?></h1>
+<h2><?= \Sustav\Funkcije::escapeOutput( $site['description'] ) ?></h2>
 <p>
-<?= \Sustav\Funkcije::escapeOutput( $site[address] ) ?><br>
-<?= \Sustav\Funkcije::escapeOutput( $site[zipcode] ) ?>,
-<?= \Sustav\Funkcije::escapeOutput( $site[city] ) ?><br>
-<?= \Sustav\Funkcije::escapeOutput( $site[country] ) ?></p>
-<?php if ( !empty( $site[phone] )) { ?>
+<?= \Sustav\Funkcije::escapeOutput( $site['address'] ) ?><br>
+<?= \Sustav\Funkcije::escapeOutput( $site['zipcode'] ) ?>,
+<?= \Sustav\Funkcije::escapeOutput( $site['city'] ) ?><br>
+<?= \Sustav\Funkcije::escapeOutput( $site['country'] ) ?></p>
+<?php if ( !empty( $site['phone'] )) { ?>
 <p><span class="uk-icon-phone"></span>
-<?= \Sustav\Funkcije::escapeOutput( $site[phone] ) ?></p>
+<?= \Sustav\Funkcije::escapeOutput( $site['phone'] ) ?></p>
 <?php } ?>
 </div>
 <?= \Sustav\Funkcije::escapeHtml( $page['content'] ) ?>
