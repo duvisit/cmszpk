2e27204095c1cf2b6d590aa963dfa15ac4b6b0a0
Pero Brbora <pero.brbora@gmail.com>
2018-03-18 19:33:27 +0100

    Add php configuration

--- php/src/sustav.php
+++ php/src/sustav.php
@@ -1,4 +1,5 @@
 <?php
+use Sustav\Postavke;
 use Sustav\Pogled\Pogled;
 use Sustav\Pogled\Sadrzaj;
 use Sustav\Upravljac\Zahtjev;
@@ -12,6 +13,13 @@ set_error_handler(function ($severity, $message, $file, $line) {
 });
 date_default_timezone_set('UTC');
 
+$logfile = __DIR__.'/error.log';
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+ini_set('log_errors', '1');
+ini_set('error_reporting', E_ALL);
+ini_set('error_log', $logfile);
+
 try {
     $zahtjev   = new Zahtjev();
     $upravljac = new Upravljac($zahtjev);
@@ -19,7 +27,6 @@ try {
     $pogled    = new Pogled($sadrzaj);
     $pogled->posalji();
 } catch (\Exception $e) {
-    $logfile = __DIR__.'/error.log';
     error_log(date(DATE_ATOM), 3, $logfile);
     error_log("\t", 3, $logfile);
     error_log($e->getMessage(), 3, $logfile);
